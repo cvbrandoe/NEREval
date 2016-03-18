69d94cc4265d3e4e0d51b0d31fe10b460c1fe9d1
cvbrandoe <carmen.brando@gmail.com>
2016-03-18 18:27:28 +0100

    structure de base pour le code

--- src/fr/matriciel/nereval.py
+++ src/fr/matriciel/nereval.py
@@ -2,4 +2,21 @@
 Created on 18 mars 2016
 
 @author: Magali, Catherine, Carmen
+@summary: Evaluation of the task of detection of Named Entity Recognition (classification is not considered)
 '''
+
+#lecture fichier gold et annoté
+
+def rappel():    
+    return 0
+
+def precision():
+    return 0
+
+def fscore():
+    return 0
+
+def err():
+    return 0
+
+# sortie indiquant où trouver les erreurs !!
