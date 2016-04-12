30d45b7e182bc6d2eb7591bd04bc4823b9324ac9
cvbrandoe <carmen.brando@gmail.com>
2016-04-12 11:56:01 +0200

    more generic algorithm, it also handles generic place annotations

--- src/fr/matriciel/NEREval.java
+++ src/fr/matriciel/NEREval.java
@@ -5,10 +5,13 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.apache.commons.lang.StringUtils;
+
+import scala.collection.mutable.HashSet;
 
 /**
  * 
@@ -16,9 +19,10 @@ import org.apache.commons.lang.StringUtils;
  * 
  * Evaluation of the task for named-entity detection (classification is not considered) according to the State of art (Nouvel et al. 2015)
  * The following is an algorithm for alignment of named-entities annotations (and their context) in two texts.
+ * - places are annotated using the following symbols: {[place1]} and/or {[#place2#]} 
  * - contents of both input files must be identical except obviously their annotations
  * - parenthesis can only be used for indicating the presence of geographic coordinates which are actually not mandatory
- * 
+ * TODO nouvel eval : difference frontières  
  */
 public class NEREval {
 	
@@ -28,10 +32,6 @@ public class NEREval {
 		String content1 = cleanUpInput(args[0]);
 		String content2 = cleanUpInput(args[1]); 
 		
-		//match context of NE in both texts
-		List<String> matches1 = matchENS(content1);
-		List<String> matches2 = matchENS(content2);
-	
 		//C : total number of annotated objects in the annotated file that are correct
 		Double c = 0.0;
 		//I : total number of adds performed by the system, in other words, objects that are not named-entities but the system considered as ones
@@ -41,18 +41,18 @@ public class NEREval {
 		//total number of named-entity (mentions?) in the gold
 		Double n = 0.0;
 			
-		Double[] te = calculateForRappel(c, d, matches1, matches2);
+		Double[] te = countMetrics(content1, content2);
 		d = te[0];
-		c = te[1];
-		i = calculateForPrecision(i, matches1, matches2);
+		c = te[1];		
 		n = te[2];
+		i = te[3];
 				
 		System.out.println();
 		System.out.println("Values for NER evaluation");
 		System.out.println("C (total number of annotated objects in the annotated file that are correct): "+c);
 		System.out.println("D (total number of omissions (deletions) performed by the system): "+d);
 		System.out.println("I (total number of adds performed by the system): "+i);
-		System.out.println("N (total number of named-entity (mentions?) in the gold): "+n); //TODO unique entities or mentions? 
+		System.out.println("N (total number of named-entity mentions in the gold): "+n); 
 		
 		// précision : le ratio entre le nombre de réponses correctes et toutes les réponses données par un système. 
 		// ou bien precision = right tagging / (right tagging + wrong tagging)
@@ -69,7 +69,7 @@ public class NEREval {
 		//f-score
 		System.out.println();
 		System.out.println("F-mesure F = 2 * ( (P * R) / (P + R) ): "+ 2 * ( (p * r) / (p + r) ));
-		//TODO here equal importance is given to recall and precision, is this what we want?
+		//here equal importance is given to recall and precision
 		
 		//errors
 		System.out.println();
@@ -78,60 +78,13 @@ public class NEREval {
 		System.out.println();
 		System.out.println("ERR(v2) = D + I / N + I: "+ (d + i) / (n + i));
 		
-		System.out.println();
-		System.out.println("SER = D + I / R (for us N) : "+ (d + i) / n );
-		//TODO here, we give igual importance to each error, again is this right for us?
+		//System.out.println();
+		//System.out.println("SER = D + I / R (for us N) : "+ (d + i) / n );
+		//here, we give equal importance to each error
 		
-		//ETER: for nested named entities (TODO could eventually be useful?)  
+		//TODO Degré de superposition  
 	}
 	
-	/**
-	 * Computes insertions performed by the system by comparing the gold and the hypothesis.
-	 * @param i
-	 * @param matches1
-	 * @param matches2
-	 * @return
-	 */
-	private static Double calculateForPrecision(Double i, List<String> matches1, List<String> matches2) {
-	
-		for (String annot : matches2) {
-			String[] valsAnnot = annot.split("\\|");
-			List<String> nesAnnot = extractENS(valsAnnot[2]);
-
-			for (String neAnnot : nesAnnot) {
-				
-				Boolean foundInAnnot2 = false;
-				for (String gold : matches1) {
-					
-					String[] valsGold = gold.split("\\|");
-					List<String> nesGold = extractENS(valsGold[2]);
-					int index = 0;
-					Boolean foundInAnnot = false;
-					while (!foundInAnnot && index < nesGold.size() ) {
-						
-						String neGold = nesGold.get(index);						
-						//match context
-						if (overlaps(Integer.parseInt(valsAnnot[0].trim()), Integer.parseInt(valsAnnot[1].trim()), 
-								Integer.parseInt(valsGold[0].trim()), Integer.parseInt(valsGold[1].trim())) ) {
-							
-							if (neAnnot.equals(neGold)) {
-								foundInAnnot = true;	
-								foundInAnnot2 = true;
-							}
-						}						
-						index++;
-					}					
-				}
-				if (!foundInAnnot2) {
-					i++;	
-					System.out.println("Wrong match, system added: "+ neAnnot +"(context:"+valsAnnot[0].trim() + " - "+ valsAnnot[1].trim() + ")");
-					System.out.println();
-				}
-			}				
-		}
-		return i;
-	}
-
 	/**
 	 * Computes correct entities detected and deletions performed by the system by comparing the gold and the hypothesis.
 	 * @param c
@@ -140,54 +93,57 @@ public class NEREval {
 	 * @param matches2
 	 * @return
 	 */
-	private static Double[] calculateForRappel(Double c, Double d, List<String> matches1, List<String> matches2) {
-		
-		Double n = 0.0;
-		for (String gold : matches1) {
-			String[] valsGold = gold.split("\\|");
-			List<String> nesGold = extractENS(valsGold[2]);
+	private static Double[] countMetrics(String content1, String content2) {
 
-			for (String neGold : nesGold) {
-				n++;
-				Boolean foundInAnnot2 = false;
-				for (String annot : matches2) {
-					
-					String[] valsAnnot = annot.split("\\|");
-					List<String> nesAnnot = extractENS(valsAnnot[2]);
-					int index = 0;
-					Boolean foundInAnnot = false;
-					String neAnnotStore = "";
-					while (!foundInAnnot && index < nesAnnot.size() ) {
-						
-						String neAnnot = nesAnnot.get(index);						
-						//match context
-						if (overlaps(Integer.parseInt(valsGold[0].trim()), Integer.parseInt(valsGold[1].trim()), 
-								Integer.parseInt(valsAnnot[0].trim()), Integer.parseInt(valsAnnot[1].trim())) ) {
-							
-							if (neGold.equals(neAnnot)) {
-								foundInAnnot = true;	
-								neAnnotStore = neAnnot;
-								foundInAnnot2 = true;
-							}
-						}						
-						index++;
-					}
-					//found match of neGold in annot
-					if (foundInAnnot) {							
-						System.out.println("Good match: gold (context:"+valsGold[0].trim() + " - "+ valsGold[1].trim() + ") is "+neGold+ 
-								" and annot (context:"+valsAnnot[0].trim() + " - "+ valsAnnot[1].trim() + ") is "+neAnnotStore);
+		//match context of NE in both texts
+		List<String> matches1 = matchENS(content1);
+		List<String> matches2 = matchENS(content2);
+		System.out.println("Nb of entities in the reference: "+matches1.size());
+		System.out.println("Nb of entities in the hypothesis: "+matches2.size());
+
+		Double n = 0.0, c = 0.0, d = 0.0, i = 0.0;
+		HashSet<Integer> seenIndices = new HashSet<Integer>();
+		for (String gold : matches1) {
+			n++;
+			String[] valsGold = gold.split("\\|");
+			String goldNE = valsGold[2];
+			Boolean found = false;
+			for (String annot : matches2) {
+				String[] valsAnnot = annot.split("\\|");
+				String annotNE = valsAnnot[2];
+				
+				if (overlaps(Integer.parseInt(valsGold[0].trim()), Integer.parseInt(valsGold[1].trim()), 
+						Integer.parseInt(valsAnnot[0].trim()), Integer.parseInt(valsAnnot[1].trim())) ) {
+					goldNE = goldNE.replaceAll("#", "");
+					if (goldNE.equals(annotNE)) {
+						found = true;
+						seenIndices.add(matches2.indexOf(annot));
+						System.out.println("Good match: gold (indices:"+valsGold[0].trim() + " - "+ valsGold[1].trim() + " indicesOrig:"+valsGold[3].trim() + " - "+ valsGold[4].trim() + ") is "+goldNE+ 
+								" and annot (indices:"+valsAnnot[0].trim() + " - "+ valsAnnot[1].trim() + " indicesOrig:"+valsAnnot[3].trim() + " - "+ valsAnnot[4].trim() + ") is "+annotNE);
 						System.out.println();
 						c++;
 					}
-				}
-				if (!foundInAnnot2) {
-					d++;
-					System.out.println("Wrong match, system ommited: "+ neGold +"(context:"+valsGold[0].trim() + " - "+ valsGold[1].trim() + ")");
-					System.out.println();
-				}
-			}				
+				}				
+			}
+			if (!found) {
+				d++;
+				System.out.println("Wrong match, system ommited: "+ goldNE +"(indices:"+valsGold[0].trim() + " - "+ valsGold[1].trim() + " indicesOrig:"+valsGold[3].trim() + " - "+ valsGold[4].trim() + ") ");
+				System.out.println();
+			}
+			
 		}
-		Double [] out = {d, c, n};
+		
+		i = new Double (matches2.size() - seenIndices.size());
+		for (int l = 0 ; l < matches2.size(); l++) {
+			if (!seenIndices.contains(l)) {	
+				String t[] = matches2.get(l).split("\\|");
+				System.out.println("Wrong match, system added: "+ t[2] +"(indices:"+t[0].trim() + " - "+ t[1].trim() + " indicesOrig:"+t[3].trim() + " - "+ t[4].trim() + ")");
+				System.out.println();
+		
+				
+			}
+		}
+		Double [] out = {d, c, n, i};
 		return out;
 	}
 
@@ -200,6 +156,13 @@ public class NEREval {
 	 * @return
 	 */
 	public static boolean overlaps(Integer ini1, Integer fin1, Integer ini2, Integer fin2) {
+		int dummy = 5;
+		ini1 = ini1 - dummy;
+		fin1 = fin1 + dummy;
+		
+		ini2 = ini2 - dummy;
+		fin2 = fin2 + dummy;
+		
 		//none overlapping
 		if ( (ini1 > ini2 && ini1 > fin2 ) && (fin1 > ini2 && fin1 > fin2) )
 			return false;
@@ -221,51 +184,35 @@ public class NEREval {
 	}
 
 	/**
-	 * Extracts list of segments containing a named-entity and its context enclosed by curly brackets, 
-	 * also information about context is given such as start and end indexes. 
+	 * Extracts named entities enclosed by square brackets and provides information about start and end indexes. 
 	 * @param contentF
 	 * @return
 	 */
 	public static List<String> matchENS(String contentF) {
 		
-		//System.out.println(contentF);
+		contentF = contentF.replaceAll("\\{", "");
+        contentF = contentF.replaceAll("\\}", "");
+		//System.out.println(contentF);		
 		List<String> result = new ArrayList<String>();
-	    String patternString = "\\{([^}]+)\\}";
+		String patternString = "\\[([^]]+)\\]";		
         Pattern pattern = Pattern.compile(patternString);
 		Matcher matcher = pattern.matcher(contentF);
 		int countC = 0;
 	    while(matcher.find()) {
 	        String valENS = matcher.group();
-	        int ini = matcher.start() - countC;
-	        countC = countC + StringUtils.countMatches(valENS, "}") + 
-	        		StringUtils.countMatches(valENS, "{") + StringUtils.countMatches(valENS, "(") + 
-	        		StringUtils.countMatches(valENS, ")") + StringUtils.countMatches(valENS, "]") +
-	        		StringUtils.countMatches(valENS, "[");
-	        int fin = matcher.end() - countC;	        
-	        result.add(ini + " | " + fin + " | " + valENS);
+	        int iniOrig = matcher.start();
+	        int finOrig = matcher.end();	     
+	        int ini = iniOrig - countC;	        
+	        countC = countC + StringUtils.countMatches(valENS, "]") +
+	        		StringUtils.countMatches(valENS, "[") +
+	        		StringUtils.countMatches(valENS, "#");	        
+	        int fin = finOrig - countC;	
+	        result.add(ini + " | " + fin + " | " + valENS+ " | " + iniOrig + " | " + finOrig);
 	        //System.out.println(ini + " - " + fin + " - " + valENS);
 	    }
 	    return result;
 	}
-	
-	/**
-	 * Extracts named entities enclosed by [].
-	 * @param contentF
-	 * @return
-	 */
-	public static List<String> extractENS(String contentF) {
 		
-		List<String> result = new ArrayList<String>();
-	    String patternString = "\\[([^]]+)\\]";
-        Pattern pattern = Pattern.compile(patternString);
-		Matcher matcher = pattern.matcher(contentF);
-	    while(matcher.find()) {
-	        String valENS = matcher.group();
-	        result.add(valENS);	       
-	    }
-	    return result;
-	}
-	
 	/**
 	 * Removes line feed and geographic coordinates in parenthesis.
 	 * @param fileN
@@ -284,6 +231,7 @@ public class NEREval {
 			}		    		    
 		    //remove coordinates
 	        String contentF = stringBuilder.toString().replaceAll("\\([^\\)]+\\)", "");
+	        contentF = contentF.replaceAll("  ", " ");
 	        return contentF;
 	        
 		} catch (IOException e) {
